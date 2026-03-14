a5dacc712d2b6bd857f873459856b9c48666fbbb
shalev2shimonov-crypto <shalev2.shimonov@gmail.com>
2026-03-14 16:09:44 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,33 +13,18 @@ class Main:
         self.updater: Updater = None
 
 def main(self):
-        import os
+import os
         port = int(os.environ.get("PORT", 9339))
         try:
             print("Server starting...")
             Server("0.0.0.0", port).start()
         except Exception as e:
             print(f"Encountered exception: {e}")
-
-                if self.crashCount >= 2:
-                    # TODO: Close all connections so that the server gets a bit faster on fixing stuff
-                    req = "yes" in input("The server has crashed 2+ times."
-                                         " Would you like to check for updates in case there's a fix? ").lower()
-                    if req: self.main()
-
-                updaterRollbackRequest: bool = "yes" in input(
-                    f"The Updater has detected that the server is in a crashed state (error: {e}).\n"
-                    "Would you like to roll-back the update? ").lower()
-
-                if updaterRollbackRequest:
-                    self.updater.performRollback()
-                    print("The roll-back has been completed successfully!")
-                    self.crashCount = 0
-                else:
-                    print("Request declined. Restarting server...\n")
-
-                self.main()
-
+            if self.crashCount >= 2:
+                exit()
+            self.crashCount += 1
+            self.main()
 
 if __name__ == '__main__':
-    Main()
+    Main().main()
+                
